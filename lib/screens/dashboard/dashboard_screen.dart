20b3b43dad95b9888f1c66448cf09ccc8e596ff7
路西法 <zhaiyuxin103@hotmail.com>
2021-08-29 14:57:46 +0800

    ✨ feat(main): storage details and chart

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:dashboard/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'components/header.dart';
+import 'components/storage_details.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -12,7 +13,28 @@ class DashboardScreen extends StatelessWidget {
       child: SingleChildScrollView(
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
-          children: [Header()],
+          children: [
+            Header(),
+            SizedBox(
+              height: defaultPadding,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Container(
+                    height: 500,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  width: defaultPadding,
+                ),
+                StorageDetails()
+              ],
+            )
+          ],
         ),
       ),
     );
