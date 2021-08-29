8bc098023b63cca434a782d8935f367737bd7398
路西法 <zhaiyuxin103@hotmail.com>
2021-08-29 11:57:07 +0800

    ✨ feat(main): side menu

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'components/side_menu.dart';
+
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              // default flex = 1
+              // and it takes 1/6 part of the screen
+              child: SideMenu(),
+            ),
+            Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: Container(color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
