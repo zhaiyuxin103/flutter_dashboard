81997c3a807888df4a53afce4126405718aa40db
路西法 <zhaiyuxin103@hotmail.com>
2021-08-29 16:16:55 +0800

    ✨ feat(main): cloud storage info card

--- lib/screens/dashboard/components/my_files.dart
+++ lib/screens/dashboard/components/my_files.dart
@@ -1,5 +1,8 @@
 import 'package:dashboard/constants.dart';
+import 'package:dashboard/models/MyFiles.dart';
 import 'package:flutter/material.dart';
+
+import 'file_info_card.dart';
 
 class MyFiles extends StatelessWidget {
   const MyFiles({
@@ -8,24 +11,43 @@ class MyFiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Column(
       children: [
-        Text(
-          "My Files",
-          style: Theme.of(context).textTheme.subtitle1,
-        ),
-        ElevatedButton.icon(
-          style: TextButton.styleFrom(
-            padding: EdgeInsets.symmetric(
-              horizontal: defaultPadding * 1.5,
-              vertical: defaultPadding,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "My Files",
+              style: Theme.of(context).textTheme.subtitle1,
             ),
-          ),
-          onPressed: () {},
-          icon: Icon(Icons.add),
-          label: Text("Add New"),
+            ElevatedButton.icon(
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.symmetric(
+                  horizontal: defaultPadding * 1.5,
+                  vertical: defaultPadding,
+                ),
+              ),
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text("Add New"),
+            ),
+          ],
         ),
+        SizedBox(
+          height: defaultPadding,
+        ),
+        GridView.builder(
+          shrinkWrap: true,
+          itemCount: demoMyFiels.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: defaultPadding,
+            childAspectRatio: 1.2,
+          ),
+          itemBuilder: (context, index) => FileInfoCard(
+            info: demoMyFiels[index],
+          ),
+        )
       ],
     );
   }
